f2a3f30ea6ea511555b9cfe5be80be61f9598bf8
tekke <tekken5953@naver.com>
2021-10-22 20:18:48 +0900

    1. 물품 상세정보 페이지 작성

---
+++ app/src/main/java/app/shoppingproject_kotlin/product/model/DetailDao.kt
@@ -0,0 +1,3 @@
+package app.shoppingproject_kotlin.product.model
+
+data class DetailDao(val p_name : String, val p_update : String, val p_index : String, val p_stock : Int, val p_price : String, val p_discount : String)
